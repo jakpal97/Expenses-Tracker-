f13b14167d96dbd49e3e6f48d40d239c1f25820c
Jakpal97 <jakub.palka97@gmail.comgit config --global user.name Jakub>
2023-05-07 17:01:54 +0200

    Adding styling changes

--- src/components/UI/ErrorBox.js
+++ src/components/UI/ErrorBox.js
@@ -1,27 +1,28 @@
-import React from "react"
+import React from 'react'
 import './ErrorBox.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons'
 
-const ErrorBox = (props)=>{
-
-
-
-return (
-    <>
-    <div className="backdrop" onClick={props.onConfirm}/>
-    <div className="modal">
-        <header className="header">
-            <h2>{props.title}</h2>
-        </header>
-        <div className="content">
-            <p>{props.text}</p>
-        </div>
-        <footer>
-            <button onClick={props.onConfirm} className="actions">Got IT</button>
-        </footer>
-    </div>
-    </>
-)
-
+const ErrorBox = props => {
+	return (
+		<>
+			<div className="backdrop" onClick={props.onConfirm} />
+			<div className="modal">
+				<header className="header">
+					<h2>{props.title}</h2>
+				</header>
+				<div className="content">
+					<p>{props.text}</p>
+					<FontAwesomeIcon icon={faCircleExclamation} />
+				</div>
+				<footer>
+					<button onClick={props.onConfirm} className="actions">
+						Got IT
+					</button>
+				</footer>
+			</div>
+		</>
+	)
 }
 
 export default ErrorBox
